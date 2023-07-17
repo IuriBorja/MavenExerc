2f5bdbaed20827b517bbc510aa5442166b720634
iuriborja_ <iuri_scorp@yahoo.com.br>
2023-07-17 14:15:23 +0100

    att prompt

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.academiadecodigo.bootcamp.Prompt;
+
 public class HelloWorld {
     public static void main(String[] args) {
 
-        Prompt prompt = new Prompt();
+        Prompt prompt = new Prompt(System.in,System.out);
 
     }
 }
